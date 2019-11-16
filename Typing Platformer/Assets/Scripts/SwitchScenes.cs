c82b3491f5b1992d67513bb8ec7be42c6deaa2c8
JLHGD <jlhgamedev@gmail.com>
2019-11-15 20:32:46 -0500

    Added SwitchScenes script

---
+++ Typing Platformer/Assets/Scripts/SwitchScenes.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SwitchScenes : MonoBehaviour
+{
+
+    #region Fields
+
+    [SerializeField]
+    private Scene scene;
+
+    #endregion Fields
+
+    #region Properties
+    #endregion Properties
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnClick(){
+        SceneManager.LoadScene (sceneName:"TitleScene");
+    }
+
+}
