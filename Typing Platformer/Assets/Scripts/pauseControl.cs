a2647c134455e012819f84882b0c499bd5148f1f
Jared Baker (RIT Student) <jdb3959@ad.rit.edu>
2019-11-15 22:06:24 -0500

    Pause Menu Functional
    Pause menu and buttons work along with escape key

---
+++ Typing Platformer/Assets/Scripts/pauseControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pauseControl : MonoBehaviour
+{
+    #region fields
+    private bool pressed;
+    [SerializeField]
+    private GameObject canvas;
+    private bool active;
+    #endregion fields
+
+    #region properties
+    public bool Pressed
+    {
+        set
+        {
+            pressed = !pressed;
+        }
+    }
+    #endregion properties
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.SetActive(false);
+        pressed = false;
+        active = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pressed == true)
+        {
+            canvas.SetActive(active);
+            pressed = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            canvas.SetActive(active);
+            active = !active;
+        }
+
+        if(canvas.activeSelf == true)
+        {
+            active = false;
+        }
+        else
+        {
+            active = true;
+        }
+    }
+}
