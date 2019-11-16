8920db0ed81e224c4560709e774fa3dfb08475c1
Ryan Cooper (RIT Student) <ric7779@ad.rit.edu>
2019-11-16 15:42:51 -0500

    level 1

---
+++ Typing Platformer/Assets/Scripts/hIDE.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hIDE : MonoBehaviour
+{
+    private int count;
+    private SpriteRenderer sr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 100;
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (count >= 0)
+            count--;
+        else
+        {
+            sr.sortingOrder = -4;
+        }
+            
+
+        
+    }
+}
