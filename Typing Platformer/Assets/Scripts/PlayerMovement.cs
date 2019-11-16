1dbcb825464e6faf41c41ae927a0db3eea8973a2
Ryan Cooper (RIT Student) <ric7779@ad.rit.edu>
2019-11-15 20:35:31 -0500

    Movement beginning

--- Typing Platformer/Assets/Scripts/PlayerMovement.cs
+++ Typing Platformer/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    #region Fields
+    [SerializeField]
+    private Vector2 pos;
+    [SerializeField]
+    private float grav;
+    [SerializeField]
+    private float accY;
+    [SerializeField]
+    private float velY;
+    [SerializeField]
+    private float velX;
+    #endregion Fields
     // Start is called before the first frame update
     void Start()
     {
-        
+        grav = -1f;
+        velX = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movin();
+    }
+
+    void Movin()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            pos.x -= velX * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            pos.x += velX * Time.deltaTime;
+        }
     }
 }
