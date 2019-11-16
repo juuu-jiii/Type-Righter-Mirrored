1e5b157aa1ffdd6849f8cfb914aa6c4306bd273d
Justin Neft (RIT Student) <jbn5379@ad.rit.edu>
2019-11-15 21:18:00 -0500

    Added in letter Prefab
    Added in a letter prefab that listens for its own input, and will activate its related block whenever the proper input is pressed.

---
+++ Typing Platformer/Assets/Scripts/MakeBlock.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeBlock : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField]
+    private KeyCode letter;
+
+    private TextMesh tm;
+
+    private bool isListening;
+
+    private float count;
+
+    [SerializeField]
+    private float timeActive = 5f;
+
+    [SerializeField]
+    private GameObject block;
+
+    #endregion Fields
+
+    #region Properties
+
+    public KeyCode Letter
+    {
+        get
+        {
+            return letter;
+        }
+        set
+        {
+            letter = value;
+        }
+    }
+
+    #endregion Properties
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tm = this.gameObject.GetComponent<TextMesh>();
+        tm.text = letter.ToString().ToUpper();
+
+        isListening = true;
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If there is no block yet, and the proper character is selected, then make the block.
+        if (Input.GetKey(letter) && isListening)
+        {
+            isListening = false;
+            // TODO: ENABLE BLOCK
+        }
+
+        // Up the count variable if the block is active.
+        if (!isListening)
+        {
+            count += Time.deltaTime;
+        }
+
+        // Check if the block should be destroyed or not.
+        if (count >= timeActive)
+        {
+            // TODO: DISABLE BLOCK
+            count = 0;
+            isListening = true;
+        }
+    }
+}
