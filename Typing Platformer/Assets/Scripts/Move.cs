b8b5dd69977be7494786ca9a0f25cb055f6b0e3b
Justin Neft (RIT Student) <jbn5379@ad.rit.edu>
2019-11-15 20:15:59 -0500

    Added Basic Folder Structure
    Added in the scripts, prefabs and media folders, and added phony starter-items so that github and unity don't delete the folders for being empty.

---
+++ Typing Platformer/Assets/Scripts/Move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+
+    #region Fields
+
+    [SerializeField]
+    private int num;
+
+    #endregion Fields
+
+    #region Properties
+
+
+
+    #endregion Properties
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
