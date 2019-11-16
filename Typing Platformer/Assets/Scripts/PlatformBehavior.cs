d0e5df68f5452515b5f25a431a9e08155562115f
Ryan Cooper (RIT Student) <ric7779@ad.rit.edu>
2019-11-15 21:05:15 -0500

    platform

---
+++ Typing Platformer/Assets/Scripts/PlatformBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformBehavior : MonoBehaviour
+{
+    #region Fields
+    [SerializeField]
+    private KeyCode key;
+    [SerializeField]
+    private bool active;
+    [SerializeField]
+    private int counter;
+    #endregion Fields
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 180;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( active == true)
+        Timer();
+    }
+
+    void Timer()
+    {
+        counter--;
+        if(counter <= 0)
+        {
+            active = false;
+            counter = 180;
+        }
+    }
+}
