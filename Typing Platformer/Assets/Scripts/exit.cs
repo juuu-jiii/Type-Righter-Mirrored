5d8fb2e6d7214305968caf2699c594f827ce2417
Jared Baker (RIT Student) <jdb3959@ad.rit.edu>
2019-11-15 21:18:59 -0500

    functional Title Scene
    added instructions canvas
    added level Select  canvas

---
+++ Typing Platformer/Assets/Scripts/exit.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class exit : MonoBehaviour
+{
+    #region fields
+    private bool pressed;
+    private float delay;
+    #endregion fields
+
+    #region properties
+    public bool Pressed
+    {
+        set
+        {
+            pressed = !pressed;
+        }
+    }
+    #endregion properties
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pressed = false;
+        delay = .5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pressed == true)
+        {
+            delay -= Time.deltaTime;
+            if (delay <= 0)
+            {
+                Application.Quit();
+            }
+        }
+
+    }
+}
