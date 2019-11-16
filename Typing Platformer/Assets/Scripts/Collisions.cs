ed47e32a9cccb495a2012d7f3d37483d99060e85
Ryan Cooper (RIT Student) <ric7779@ad.rit.edu>
2019-11-15 22:02:21 -0500

    s

---
+++ Typing Platformer/Assets/Scripts/Collisions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collisions : MonoBehaviour
+{
+    #region Fields
+    [SerializeField]
+    private GameObject[] blocks;
+    #endregion Fields
+
+    #region Properties
+    public GameObject[] Blocks
+    {
+        get { return blocks; }
+        set { blocks = value; }
+    }
+    #endregion Properties
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject collided = collision.gameObject;
+        collided.GetComponent<>
+    }
+}
